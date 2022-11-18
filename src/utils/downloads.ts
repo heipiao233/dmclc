fe16843bb5ea0555a913f334088d3105cffee45c
heipiao233 <heipiao233@outlook.com>
2022-11-18 15:20:27 +0800

    Use got to implement http_request and download

--- src/utils/downloads.ts
+++ src/utils/downloads.ts
@@ -1,42 +1,25 @@
 import fs from "fs";
-import fr from "follow-redirects";
-const https = fr.https;
-export async function downloadAll (files: Map<string, fs.PathLike>, mirror?: string): Promise<void> {
+import got from "got";
+export async function downloadAll(files: Map<string, fs.PathLike>, mirror?: string): Promise<void> {
     const promises: Array<Promise<void>> = new Array<Promise<void>>();
     files.forEach((v, k) => {
         promises.push(download(k, v, mirror));
     });
     await Promise.all(promises);
 }
-export async function download (url: string, filename: fs.PathLike, mirror?: string): Promise<void> {
-    return await new Promise<void>((resolve) => {
-        if (mirror !== undefined) {
-            url = url.replaceAll("launchermeta.mojang.com", mirror)
-                .replaceAll("launcher.mojang.com", mirror)
-                .replaceAll("resources.download.minecraft.net", mirror + "/assets")
-                .replaceAll("libraries.minecraft.net", mirror + "/maven")
-                .replaceAll("files.minecraftforge.net", mirror)
-                .replaceAll("maven.minecraftforge.net", mirror + "/maven")
-                .replaceAll("meta.fabricmc.net", mirror + "/fabric-meta")
-                .replaceAll("maven.fabricmc.net", mirror + "/maven")
-                .replaceAll("piston-meta.mojang.com", mirror)
-                .replaceAll("piston-data.mojang.com", mirror);
-        }
-        console.log(url);
-        const file = fs.createWriteStream(filename);
-        const req = https.request(url)
-            .on("response", (res) => {
-                res.on("data", (chunk) => {
-                    file.write(chunk);
-                })
-                    .on("end", () => {
-                        file.close();
-                        resolve();
-                    });
-            })
-            .on("error", (error) => {
-                console.error(error);
-            });
-        req.end();
-    });
+export async function download(url: string, filename: fs.PathLike, mirror?: string): Promise<void> {
+    if (mirror !== undefined) {
+        url = url.replaceAll("launchermeta.mojang.com", mirror)
+            .replaceAll("launcher.mojang.com", mirror)
+            .replaceAll("resources.download.minecraft.net", mirror + "/assets")
+            .replaceAll("libraries.minecraft.net", mirror + "/maven")
+            .replaceAll("files.minecraftforge.net", mirror)
+            .replaceAll("maven.minecraftforge.net", mirror + "/maven")
+            .replaceAll("meta.fabricmc.net", mirror + "/fabric-meta")
+            .replaceAll("maven.fabricmc.net", mirror + "/maven")
+            .replaceAll("piston-meta.mojang.com", mirror)
+            .replaceAll("piston-data.mojang.com", mirror);
+    }
+    const file = fs.createWriteStream(filename);
+    (await got(url)).pipe(file);
 }
--- src/utils/http_request.ts
+++ src/utils/http_request.ts
@@ -1,31 +1,13 @@
-import * as https from "https";
+import got from "got";
 
 export async function get (url: string, mirror: string): Promise<string> {
-    return await new Promise(resolve => {
-        url = url.replaceAll("launchermeta.mojang.com", mirror)
-            .replaceAll("launcher.mojang.com", mirror)
-            .replaceAll("resources.download.minecraft.net", mirror)
-            .replaceAll("libraries.minecraft.net", mirror + "/maven")
-            .replaceAll("files.minecraftforge.net", mirror)
-            .replaceAll("meta.fabricmc.net", mirror + "/fabric-meta")
-            .replaceAll("maven.fabricmc.net", mirror + "/maven")
-            .replaceAll("piston-meta.mojang.com", mirror);
-        console.log(url);
-        const req = https.request(url);
-        req.on("response", (res) => {
-            if (res.statusCode !== 200) {
-                console.error("http error:" + url);
-            } else {
-                const all: Buffer[] = [];
-                res.on("data", (data) => {
-                    all.push(data);
-                });
-                res.on("end", () => {
-                    const content = all.join("");
-                    resolve(content);
-                });
-            }
-        });
-        req.end();
-    });
+    url = url.replaceAll("launchermeta.mojang.com", mirror)
+        .replaceAll("launcher.mojang.com", mirror)
+        .replaceAll("resources.download.minecraft.net", mirror)
+        .replaceAll("libraries.minecraft.net", mirror + "/maven")
+        .replaceAll("files.minecraftforge.net", mirror)
+        .replaceAll("meta.fabricmc.net", mirror + "/fabric-meta")
+        .replaceAll("maven.fabricmc.net", mirror + "/maven")
+        .replaceAll("piston-meta.mojang.com", mirror);
+    return (await got(url)).read();
 }
