e3e6eac9b887f0deb9af03985b3187d4d2640341
heipiao233 <heipiao233@outlook.com>
2022-11-23 14:07:56 +0800

    fix

---
+++ src/utils/expand_inherits_from.ts
@@ -0,0 +1,12 @@
+import { McInstallation } from "../schemas.js";
+import { merge } from "./mergeversionjson.js";
+import fs from "fs";
+
+export function expandInheritsFrom(versionObject: McInstallation, rootPath: string): McInstallation {
+    let ret: McInstallation = versionObject;
+    if(versionObject.inheritsFrom!==undefined){
+        const original = JSON.parse(fs.readFileSync(`${rootPath}/versions/${versionObject.inheritsFrom}/${versionObject.inheritsFrom}.json`).toString());
+        ret = merge(expandInheritsFrom(original, rootPath), versionObject);
+    }
+    return ret;
+}
