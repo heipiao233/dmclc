8c2f22fd1bf2dc9b460e5803c13c3267aba6884f
heipiao233 <heipiao233@outlook.com>
2022-11-28 20:13:21 +0800

    better java locating

--- src/utils/findjava.ts
+++ src/utils/findjava.ts
@@ -3,40 +3,73 @@
 import * as cp from "child_process";
 import os from "os";
 import fs from "fs";
+import { promisified as regedit } from "regedit";
 
-export async function findAllJava(): Promise<Map<string, string>> {
-    const res = new Map<string, string>();
+export async function findAllJava(): Promise<string[][]> {
     switch (os.platform()) {
     case "win32":
-        return await readFromRegister();
+        return await findForWindows();
     case "linux":
         return await findForLinux();
+    case "darwin":
+        return await findForMac();
     default:
-        break;
+        return [];
     }
-    return res;
 }
 
-async function readFromRegister(): Promise<Map<string, string>> {
-    const lines = cp.execSync("reg query \"HKCR\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\MuiCache\" /f \"java.exe.ApplicationCompany\"").toString().split("\n");
-    const ret = new Map<string, string>();
-    await Promise.all(lines.filter(i=>i.includes("java.exe")).map(async i=>{
-        const javaExec = i.match("[A-Z]:.+?\\.exe")![0];
-        ret.set(javaExec, await getJavaVersion(javaExec));
-    }));
+async function findForWindows(): Promise<string[][]> {
+    const ret:string[][] = [];
+    ret.push(...await readFromRegister("HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Runtime Environment"));
+    ret.push(...await readFromRegister("HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Development Kit"));
+    ret.push(...await readFromRegister("HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\JRE"));
+    ret.push(...await readFromRegister("HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\JDK"));
+    for (const pf of [
+        process.env["ProgramFiles"]??"C:\\Program Files",
+        process.env["ProgramFiles(x86)"]??"C:\\Program Files (x86)",
+        process.env["ProgramFiles(ARM)"]??"C:\\Program Files (ARM)"
+    ]) {
+        if(!fs.existsSync(pf)){
+            continue;
+        }
+        for(const vendor of ["Java", "BellSoft", "AdoptOpenJDK", "Zulu", "Microsoft", "Eclipse Foundation", "Semeru"]) {
+            for (const version of fs.readdirSync(`${pf}\\${vendor}`)) {
+                const exec = `${pf}\\${vendor}\\${version}\\bin\\java.exe`;
+                if(fs.existsSync(exec))ret.push([version, exec]);
+            }
+        }
+    }
     return ret;
 }
-async function findForLinux(): Promise<Map<string, string>> {
+
+async function readFromRegister(key: string): Promise<string[][]> {
+    const versions = await regedit.list([key]);
+    const ret: string[][] = [];
+    versions[key].keys.forEach(async val=>{
+        const query = `${key}\\${val}`;
+        const versionInfo = (await regedit.list([query]))[query];
+        ret.push([val, `${versionInfo.values["JavaHome"].value}\\bin\\java.exe`]);
+    });
+    return ret;
+}
+
+async function findForLinux(): Promise<string[][]> {
     const dirs = ["/usr/java", "/usr/lib/jvm", "/usr/lib32/jvm"];
-    const ret = new Map<string, string>();
+    const ret: string[][] = [];
     for (const i of dirs) {
         for (const j of fs.readdirSync(i)) {
             const exec = `${i}/${j}/bin/java`;
             if(fs.existsSync(exec)){
-                ret.set(exec, await getJavaVersion(exec));
+                ret.push([exec, await getJavaVersion(exec)]);
             }
         }
     }
+    return ret;
+}
+
+async function findForMac(): Promise<string[][]> {
+    const ret: string[][] = [];
+    // TODO: MAC
     return ret;
 }
 
