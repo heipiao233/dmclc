a394e3c37b877f5bfebefefbba4d0961001b1ea1
heipiao233 <heipiao233@outlook.com>
2023-01-22 15:22:51 +0800

    fix

--- src/utils/MergeVersionJSONs.ts
+++ src/utils/MergeVersionJSONs.ts
@@ -1,12 +1,15 @@
+import assert from "assert";
 import { MCVersion } from "../schemas.js";
 
 export function merge(a: MCVersion, b: MCVersion, mergeID = false): MCVersion {
     const c = a;
-    if(c.minecraftArguments!=undefined){
+    if("minecraftArguments" in c){
+        assert("minecraftArguments" in b);
         c.minecraftArguments = b.minecraftArguments;
-    }else if(c.arguments!=undefined){
-        c.arguments.game?.push(...b.arguments!.game??[]);
-        c.arguments.jvm?.push(...b.arguments!.jvm??[]);
+    } else {
+        assert("arguments" in b);
+        c.arguments.game?.push(...b.arguments.game??[]);
+        c.arguments.jvm?.push(...b.arguments.jvm??[]);
     }
     c.libraries.unshift(...b.libraries);
     c.mainClass = b.mainClass;
